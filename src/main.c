44fb34db993d225d69b282d730922aac4ec58d82
salvaCasta <salCas276@gmail.com>
2022-06-01 16:07:25 -0300

    Made changes to the tcpConnectionHandler

--- src/main.c
+++ src/main.c
@@ -12,61 +12,105 @@
 #include "selector.h"
 #include "stm.h"
 #include "buffer.h"
+
 #define MAX_PENDING_CONNECTIONS   3    // un valor bajo, para realizar pruebas
 #define PORT 8888
 #define OTHERPORT 9090
 
+void tcpConnectionHandler(struct selector_key *key);
+void readSocketHandler(struct selector_key *key);
+void writeSocketHandler(struct selector_key *key);
+
 struct buffer_and_fd{
     int fd;
-    buffer * buffer1;
+    buffer * wBuff;
+    buffer * rBuff;
+};
+
+//Handlers estandares para sockets activos y pasivos
+fd_handler passiveFdHandler = {
+    .handle_read = &tcpConnectionHandler,
+    .handle_write = NULL,
+    .handle_block = NULL,
+    .handle_close = NULL
+};
+
+fd_handler activeFdHandler = {
+    .handle_read = &readSocketHandler,
+    .handle_write = &writeSocketHandler,
+    .handle_block = NULL,
+    .handle_close = NULL
 };
 
 
-
-void reading_from_client_socket      (struct selector_key *key){
+void readSocketHandler(struct selector_key *key){
     struct buffer_and_fd * b = (struct buffer_and_fd*)key->data- ;
     char temp[1024];
     int nread;
     if( buffer_can_write() ){
         nread = recv(b->fd,temp,1024,MSG_DONTWAIT);
         buffer_write(nread,temp);
-        selector_register(key->s, newSocket->fd, reading_from_client_socket(),OP_READ,&newSocket);
+        selector_register(key->s, newSocket->fd, &readSocketHandler,OP_READ,&newSocket);
     }
 }
 
-void writing_on_dest_socket(struct selector_key * key ){
+
+void writeSocketHandler(struct selector_key * key ){
 
 }
 
 
-void handle_read_connect      (struct selector_key *key){
-    sockaddr otherData;
-    socklen_t  otherDataSize = sizeof (otherData);
-    struct buffer_and_fd * newSocket = (struct buffer_and_fd * )key->data;
-    * newSocket = accept(key->fd,&otherData,&otherDataSize);
+
+//El cliente se conecta conmigo a traves del socket pasivo tcp (masterSocket)
+//
+//  (masterSock)
+// Cli -----> Proxy
+//
+//Abro un socket activo tcp (cliSock) para comunicarme con este
+//y un socket activo tcp (serSock) para comunicarme con el server destino
+//
+//   (cliSock)   (serSock)
+// Cli <---> Proxy <---> Server
+
+//TODO: ver como manejar errores de las syscalls!!!
+void tcpConnectionHandler(struct selector_key *key){
+
+    //Abro socket para comunicarme con el cliente
+    struct sockaddr_in cliSockAddr;
+    socklen_t cliSockAddrSize= sizeof(cliSockAddr);
+
+    //TODO: asegurar que el accept no bloquee
+    int cliSockFd = accept(key->fd, &cliSockAddr, &cliSockAddrSize);
+
+    //TODO: mirar que hacer con el campo data (el ultimo)
+    selector_register(key->s, cliSockFd, &activeFdHandler, OP_READ | OP_WRITE, NULL);
 
 
-    struct sockaddr_in connectionData = {.sin_family=AF_INET,.sin_addr.s_addr=inet_addr("127.0.0.1"),.sin_port=htons(OTHERPORT)};
-    int serverSocketFd = connect();
+    //Abro socket para comunicarme con el server
+    int serSockFd = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in serSockAddr = {.sin_family = AF_INET,
+        .sin_addr.s_addr = inet_addr("127.0.0.1"),
+        .sin_port = htons(OTHERPORT)};
 
-    selector_register(key->s, newSocket->fd, reading_from_client_socket(),OP_READ,&newSocket);
-    selector_register(key->,serverSocketFd,writing_on_dest_socket,OP_WRITE,&newSocket);
+    connect(serSockFd, &serSockAddr, sizeof(serSockAddr));
 
+    //TODO: mirar que hacer con el campo data (el ultimo)
+    selector_register(key->s, serSockFd, &activeFdHandler, OP_READ | OP_WRITE, NULL);
 }
 
 
 int main(){
-    int master_socket[2];
-    int master_socket_size=0;
+    int masterSocket[2];
+    int masterSocketSize=0;
 
-    if( (master_socket[master_socket_size] = socket(AF_INET , SOCK_STREAM , 0)) == 0)
+    if( (masterSocket[masterSocketSize] = socket(AF_INET , SOCK_STREAM , 0)) == 0)
     {
         goto error;
     } else {
         //set master socket to allow multiple connections , this is just a good habit, it will work without this
-        if( setsockopt(master_socket[master_socket_size], SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)) < 0 )
+        if( setsockopt(masterSocket[masterSocketSize], SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)) < 0 )
         {
-            goto errror;
+            goto error;
         }
 
         //type of socket created
@@ -75,33 +119,34 @@ int main(){
         address.sin_port = htons( PORT );
 
         // bind the socket to localhost port 8888
-        if (bind(master_socket[master_socket_size], (struct sockaddr *)&address, sizeof(address))<0)
+        if (bind(masterSocket[masterSocketSize], (struct sockaddr *)&address, sizeof(address))<0)
         {
-            close(master_socket[master_socket_size]);
+            close(masterSocket[masterSocketSize]);
             goto error;
         }
         else {
-            if (listen(master_socket[0], MAX_PENDING_CONNECTIONS) < 0)
+            if (listen(masterSocket[0], MAX_PENDING_CONNECTIONS) < 0)
             {
-                close(master_socket[master_socket_size]);
+                close(masterSocket[masterSocketSize]);
                 goto error;
             } else {
-                master_socket_size++;
+                masterSocketSize++;
             }
         }
     }
 
     buffer * buffer1 ;
     char bufferArray[1024];
-    buffer_init(&buffer1,1024,&bufferArray);
+    buffer_init(&buffer1, 1024, &bufferArray);
 
 
 
     size_t  initial_elements = 1 ;
     fd_selector fdSelector = selector_new(initial_elements);
+
     struct buffer_and_fd newClient;
-    fd_handler  connectHandler ={.handle_read=handle_read_connect};
-    selector_register(fdSelector,master_socket[0],    connectHandler,    OP_READ , &newClient);
+
+    selector_register(fdSelector, masterSocket[0], &passiveFdHandler, OP_READ, &newClient);
     selector_select(fdSelector);
 
     error:
