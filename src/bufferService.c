48e8fcba5710d2ce4de788cad8d59aa5b200b4b2
salvaCasta <salCas276@gmail.com>
2022-06-02 15:30:10 -0300

    Added basic version of bufferService

--- src/bufferService.c
+++ src/bufferService.c
@@ -1,58 +1,79 @@
 #include "bufferService.h"
+#include <stdint.h>
 
-bufferAndFd* buffers[MAX_SIZE];
+bufferAndFd* buffers[MAX_SIZE] = {0};
 
-bufferAndFd * getBuffer(int fd){
-    if(fd < 0)
-        return NULL;
-    for(int i = 0; i < MAX_SIZE; i++){
-        if(buffers[i] != NULL && buffers[i]->fd == fd){
-            return buffers[i];
-        }
-    }
-    return NULL;
-}
-
-int linkBufferFor(int fd, fd_interest interest){
-
-}
-
-int unlinkBufferFor(int fd, fd_interest interest);
-
-//To perform this operation both fds shall and shall only have writing buffers linked
-//The writing buffer of each will be asigned to the reading buffer of the other
-int crossLinkBuffer(int fd1, int fd2);
-
-//Neither fd should be linked to any buffer
-//Both fds will be linked and crossed with each other
-int linkAndCrossBuffer(int fd1, int fd2){
-    if(isLinkedFor(fd1) || isLinkedFor(fd2))
-    if(linkBufferFor(fd1, OP_WRITE) == -1){
+int linkBuffer(int fd){
+    if(buffers[fd] != NULL)
         return -1;
-    }
 
-    if(linkBufferFor(fd2, OP_WRITE) == -1){
-        unlinkBufferFor(fd2, )
-    }
+    buffers[fd] = malloc(sizeof(bufferAndFd));
+    buffers[fd]->fd = fd;
+    buffers[fd]->linkedFd = -1;
+
+    buffer * wBuff = malloc(sizeof(buffer));
+    buffer * rBuff = malloc(sizeof(buffer));
+
+    uint8_t * wData = malloc(BUFFER_SIZE);
+    uint8_t * rData = malloc(BUFFER_SIZE);
+
+    buffer_init(wBuff, BUFFER_SIZE, wData);
+    buffer_init(rBuff, BUFFER_SIZE, rData);
+
+    buffers[fd]->wBuff = wBuff;
+    buffers[fd]->rBuff = rBuff;
+
+    return 0;
 }
 
-static int getFreePosition(){
-    for(int i = 0; i < MAX_SIZE; i++){
-        if(buffers[i] == NULL){
-            return i;
-        }
-    }
-    return -1;
+int unlinkBufferFor(int fd){
+
 }
 
-//To check if fd is linked (for anything) use interest = OP_NOOP
-static int isLinkedFor(int fd, fd_interest interest){
-    if(fd < 0)
+int crossLinkBuffers(int fd1, int fd2){
+    if(buffers[fd1] != NULL || buffers[fd2] != NULL)
+        return -1;
+
+    buffers[fd1] = malloc(sizeof(bufferAndFd));
+    buffers[fd1]->fd = fd1;
+    buffers[fd1]->linkedFd = fd2;
+
+    buffers[fd2] = malloc(sizeof(bufferAndFd));
+    buffers[fd2]->fd = fd2;
+    buffers[fd2]->linkedFd = fd1;
+
+    buffer * wBuff = malloc(sizeof(buffer));
+    buffer * rBuff = malloc(sizeof(buffer));
+
+    uint8_t * wData = malloc(BUFFER_SIZE);
+    uint8_t * rData = malloc(BUFFER_SIZE);
+
+    buffer_init(wBuff, BUFFER_SIZE, wData);
+    buffer_init(rBuff, BUFFER_SIZE, rData);
+
+    buffers[fd1]->wBuff = wBuff;
+    buffers[fd2]->rBuff = rBuff;
+
+    buffers[fd2]->wBuff = rBuff;
+    buffers[fd2]->rBuff = wBuff;
+
+    return 0;
+}
+
+buffer * getWriteBuffer(int fd){
+    if(fd < 0 || buffers[fd] == NULL)
         return NULL;
-    for(int i = 0; i < MAX_SIZE; i++){
-        if(buffers[i] != NULL && buffers[i]->fd == fd){
-            return buffers[i];
-        }
-    }
-    return NULL;
+    return buffers[fd]->wBuff;
+}
+
+buffer * getReadBuffer(int fd){
+    if(fd < 0 || buffers[fd] == NULL)
+        return NULL;
+    return buffers[fd]->rBuff;
+}
+
+int getLinkedFd(int fd){
+    if(fd < 0 || buffers[fd] == NULL)
+        return NULL;
+    return buffers[fd]->linkedFd;
 }
--- src/bufferService.h
+++ src/bufferService.h
@@ -5,25 +5,27 @@
 #include <selector.h>
 
 #define MAX_SIZE 2048
+#define BUFFER_SIZE 512
 
 typedef struct bufferAndFd{
     int fd;
+    int linkedFd;
     buffer * wBuff;
     buffer * rBuff;
 } bufferAndFd;
 
-bufferAndFd * getBuffer(int fd);
+int linkBuffer(int fd);
 
-int linkBufferFor(int fd, fd_interest interest);
+int unlinkBuffer(int fd);
 
-int unlinkBufferFor(int fd, fd_interest interest);
-
-//To perform this operation both fds shall and shall only have writing buffers linked
-//The writing buffer of each will be asigned to the reading buffer of the other
 int crossLinkBuffers(int fd1, int fd2);
 
-//Neither fd should be linked to any buffer
-//Both fds will be linked and crossed with each other
-int linkAndCrossBuffers(int fd1, int fd2);
+buffer * getWriteBuffer();
+
+buffer * getReadBuffer();
+
+int getFd();
+
+int getLinkedFd();
 
 #endif
