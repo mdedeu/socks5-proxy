d152dc5775e589255d52c2a645f4e2f908d80259
mdedeu <mdedeu@itba.edu.ar>
2022-06-07 00:04:05 -0300

    Parser signature

--- src/parsers/parserCoolProtocolAuth.h
+++ src/parsers/parserCoolProtocolAuth.h
@@ -1,6 +1,21 @@
 #ifndef PARSER_H_AUTH_COOLPROTOCOL
 #define PARSER_H_AUTH_COOLPROTOCOL
 
+struct coolProtocolAuthenticationMessage{
+	uint8_t version;
+	uint8_t ulen;
+	char* uname;
+	uint8_t passlen;
+	char* pass;
+};
 
+struct coolProtocolAuthenticationResponse{
+	uint16_t status;
+}
+
+
+struct coolProtocolAuthenticationMessage * init_coolProtocolAuthenticationMessage_parser();
+bool feed_coolProtocolAuthenticationMessage_parser(struct coolProtocolAuthenticationMessage * sock_data , char * input, int input_size);
+void close_coolProtocolAuthenticationMessage_parser(struct coolProtocolAuthenticationMessage * message);
 
 #endif
---
+++ src/parsers/parserCoolProtocolRequestListener.h
@@ -0,0 +1,70 @@
+#ifndef PARSER_H_REQUEST_LISTENER_COOLPROTOCOL
+#define PARSER_H_REQUEST_LISTENER_COOLPROTOCOL
+
+struct coolProtocolRequestMessage{
+	
+	uint8_t action;
+	uint8_t method;
+
+};
+
+struct coolProtocolRequestMessageAddUser{
+	
+	struct* coolProtocolRequestMessage;
+	uint8_t ulen;
+	char* uname;
+	uint8_t passlen;
+	char* pass;
+
+};
+
+struct coolProtocolRequestMessageRemoveUser{
+	
+	struct* coolProtocolRequestMessage;
+	uint8_t ulen;
+	char* uname;
+
+};
+
+
+struct coolProtocolRequestMessagePassSpoofing{
+	
+	struct* coolProtocolRequestMessage;
+	uint8_t pcol;
+
+};
+
+struct coolProtocolRequestMessageChangeBufferSize{
+	
+	struct* coolProtocolRequestMessage;
+	uint16_t nsize;
+		
+};
+
+//parser intializations
+
+struct coolProtocolRequestMessage * init_coolProtocolRequest_parser();
+struct coolProtocolRequestMessageAddUser * init_coolProtocolRequestAddUser_parser();
+struct coolProtocolRequestMessageRemoveUser * init_coolProtocolRequestRemoveUser_parser();
+struct coolProtocolRequestMessagePassSpoofing * init_coolProtocolRequestPassSpoofing_parser();
+struct coolProtocolRequestMessageChangeBufferSize * init_coolProtocolRequestChangeBufferSize_parser();
+
+//parser feeds
+
+bool feed_coolProtocolRequestMessage_parser(struct coolProtocolRequestMessage * sock_data , char * input, int input_size);
+bool feed_coolProtocolRequestMessageAddUser_parser(struct coolProtocolRequestMessageAddUser* sock_data , char * input, int input_size);
+bool feed_coolProtocolRequestMessageRemoveUser_parser(struct coolProtocolRequestMessageRemoveUser * sock_data , char * input, int input_size);
+bool feed_coolProtocolRequestMessagePassSpoofing_parser(struct coolProtocolRequestMessagePassSpoofing * sock_data , char * input, int input_size);
+bool feed_coolProtocolRequestMessageChangeBufferSize_parser(struct coolProtocolRequestMessageChangeBufferSize * sock_data , char * input, int input_size);
+
+
+//parser closing methods
+
+void close_coolProtocolRequestMessage_parser(struct coolProtocolRequestMessage * message);
+void close_coolProtocolRequestMessageAddUser_parser(struct coolProtocolRequestMessageAddUser * message);
+void close_coolProtocolRequestMessageRemoveUser_parser(struct coolProtocolRequestMessageRemoveUser * message);
+void close_coolProtocolRequestMessagePassSpoofing_parser(struct coolProtocolRequestMessagePassSpoofing * message);
+void close_coolProtocolRequestMessageChangeBufferSize_parser(struct coolProtocolRequestMessageChangeBufferSize * message);
+
+
+#endif
