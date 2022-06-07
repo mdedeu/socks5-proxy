22fc7346742a4baef327506315c193ef18ad4f4c
salvaCasta <salCas276@gmail.com>
2022-06-07 00:46:50 -0300

    Fixed bug in authenticated.c

--- src/state/authenticated.c
+++ src/state/authenticated.c
@@ -4,28 +4,29 @@
 unsigned authenticated_handler_read(struct selector_key *key){
     sock_client *client_data = (sock_client *) key->data;
 
+    buffer_compact(client_data->write_buffer);
     if(!buffer_can_write(client_data->write_buffer))
         return AUTHENTICATED;
 
     size_t available_space;
-    buffer_compact(client_data->write_buffer);
     uint8_t * write_from = buffer_write_ptr(client_data->write_buffer, &available_space);
 
     int received_amount = recv(key->fd, write_from, available_space, MSG_DONTWAIT);
-    buffer_write_adv(client_data->write_buffer, received_amount);
 
     bool finished = feed_sock_request_parser(
         (struct sock_request_message *) (client_data->current_parser.request_message),
         write_from,
         received_amount
     );
+
+    //TODO: mirar que hacer en el caso en el que el cliente escriba de mas y este moviendo mal el puntero de lectura
+    buffer_write_adv(client_data->write_buffer, received_amount);
+    buffer_read_adv(client_data->write_buffer, received_amount);
+    buffer_compact(client_data->write_buffer);
     
-    if(!finished){
+    if(!finished)
         return AUTHENTICATED;
-    }
-    else {
-        process_request_message(client_data->current_parser.request_message, key);
-        return READY_TO_CONNECT;
-    }
-    return AUTHENTICATED;
+
+    process_request_message(client_data->current_parser.request_message, key);
+    return READY_TO_CONNECT;
 }
