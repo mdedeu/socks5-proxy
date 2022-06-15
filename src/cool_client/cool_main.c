2c25083de92a400890638ffe595ba93cca81d864
gbeade <gbeade@itba.edu.ar>
2022-06-14 23:25:30 -0300

    Changed client command format and added parameter validations

--- src/cool_client/cool_main.c
+++ src/cool_client/cool_main.c
@@ -8,6 +8,7 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <signal.h>
+#include <errno.h>
 #include "../parsing/cool_client_parsing/simpleResponseParser.h"
 #include "../parsing/cool_client_parsing/generalResponseParser.h"
 
@@ -30,6 +31,8 @@
 #define TOTAL_BYTES_SENT 3
 #define TOTAL_BYTES_RECV 4
 #define CONNECTED_USERS 5
+#define MAX_AUTH_TRIES 3
+#define COMMAND_MAX_LEN 128
 
 static int ask_method_and_parameters(uint8_t * action, uint8_t * method, uint8_t * parameters);
 static int ask_parameters(uint8_t method, uint8_t * parameters);
@@ -42,8 +45,29 @@ static int send_method_and_parameters(int sock_fd, uint8_t action, uint8_t metho
 static int send_array(uint8_t socket_fd, uint8_t len, uint8_t * array);
 static void print_status(uint16_t status);
 static void print_response(uint8_t action, uint8_t method, uint8_t response_length, char * response);
+static void print_help();
+static int close_connection(int socket_fd);
+static void print_welcome();
+static int resolve_command(char * command, uint8_t * method, uint8_t * action, uint8_t * parameters);
 
-int main(){
+#define QUERIES_TOTAL 7
+#define MODIFIERS_TOTAL 5
+char * queries[] = {"gthc", "gcc", "gmcc", "gtbs", "gnuc", "gmbs", "gul"};
+char * modifiers[] = {"au", "ru", "eps", "dps", "cbs"};
+
+int main(int argc, char * argv[]){
+    if(argc != 2)
+        return -1;
+
+    errno = 0;
+    int address_family = strtol(argv[1], NULL, 10);
+
+    if(errno != 0)
+        return -1;
+    if(address_family != 4 && address_family != 6)
+        return -1;
+
+
     uint8_t username[CREDS_LEN] = {0}, password[CREDS_LEN] = {0};
     uint8_t action, method;
     uint8_t parameters[PARAMS_LEN] = {0};
@@ -65,9 +89,8 @@ int main(){
     if(connect(sock_fd, (struct sockaddr *) &server_address_4, sizeof(server_address_4)) < 0)
         return 1;
 
+    int tries=0;
     while(returned_status != 0xC001){
-
-    printf("\n~~~~~~~~~~~Authentication~~~~~~~~~~~\n\n");
 
     ask_credentials(username, password);
 
@@ -89,11 +112,14 @@ int main(){
     close_simple_response_parser(simple_response);
 
     putchar('\n');
+
+    if(++tries >= MAX_AUTH_TRIES){
+        printf("Max number of tries reached, closing client\n.");
+        return close_connection(sock_fd);
+    }
     }
 
     while(1){
-
-    printf("\n~~~~~~~~~~~Action & Method~~~~~~~~~~~\n\n");
 
     int param_len = ask_method_and_parameters(&action, &method, parameters);
 
@@ -117,31 +143,21 @@ int main(){
 }
 
 static int ask_method_and_parameters(uint8_t * action, uint8_t * method, uint8_t * parameters){
-    uint8_t action_str[6];
-    uint8_t method_str[6];
-    int len;
+    char command[COMMAND_MAX_LEN];
+    printf("> ");
 
-    printf("Action: ");
     fflush(stdout);
-    if(fgets((char *) action_str, 6, stdin) == 0)
-        return -1;
-    action_str[4]=0;
-    *action = strtol((char *) action_str, NULL, 0);
+     if(fgets(command, COMMAND_MAX_LEN-1, stdin) == 0)
+         return -1;
+     command[strlen(command)-1]=0;
 
-    printf("Method: ");
-    fflush(stdout);
-    if(fgets((char *) method_str, 6, stdin) == 0)
-        return -1;
-    method_str[4]=0;
-    *method = strtol((char *) method_str, NULL, 0);
-
-    if(*action == QUERY)
-        return 0;
-
-    if( (len = ask_parameters(*method, parameters)) < 0)
+    if(resolve_command(command, action, method, parameters) == -1)
         return -1;
     
-    return len;
+    if(*parameters)
+        return ask_parameters(*method, parameters);
+
+    return 0;
 }
 
 //Returns the number of bytes in the parameters and -1 in case of error
@@ -192,7 +208,7 @@ static int ask_credentials(uint8_t * username, uint8_t * password){
 }
 
 static int ask_username(uint8_t * username){
-    printf("Username: ");
+    printf("Enter username: ");
     fflush(stdout);
     if(fgets((char *) username, CREDS_LEN, stdin) == 0)
         return -1;
@@ -200,7 +216,7 @@ static int ask_username(uint8_t * username){
 }
 
 static int ask_password(uint8_t * password){
-    printf("Password: ");
+    printf("Enter password: ");
     fflush(stdout);
     if(fgets((char *) password, CREDS_LEN, stdin) == 0)
         return -1;
@@ -268,11 +284,11 @@ static int send_array(uint8_t socket_fd, uint8_t len, uint8_t * array){
 
 static void print_status(uint16_t status){
     if(status == 0xC001)
-        printf("\nAuthentication Successful!");
+        print_welcome();
     else if(status == 0x4B1D)
-        printf("\nAuthentication Failed");
+        printf("Invalid credentials, please try again.\n");
     else{
-        printf("\nPlease enter valid credentials");
+        printf("Please enter valid credentials\n");
     }
 }
 
@@ -281,4 +297,36 @@ static void print_response(uint8_t action, uint8_t method, uint8_t response_leng
     //printf("Method: %X\n", method);
     *(response + response_length) = 0;
     printf("\nResponse: %d\n", response[0]);
+}
+
+static int close_connection(int socket_fd){
+    return close(socket_fd);
+}
+
+static void print_welcome(){
+    printf("Welcome to the cool sock5 proxy configuration\nEnter the help command in case you need any help\n");
+}
+
+//If the command is invalid it returns -1, else 0
+static int resolve_command(char * command, uint8_t * method, uint8_t * action, uint8_t * parameters){
+
+    for(int i = 0; i < QUERIES_TOTAL; i++){
+        if(!strcmp(command, queries[i])){
+            *method = QUERY;
+            *action = i+1;
+            *parameters = 0;
+            return 0;
+        }
+    }
+
+    for(int i = 0; i < MODIFIERS_TOTAL; i++){
+        if(!strcmp(command, modifiers[i])){
+            *method = MODIFY;
+            *action = i;
+            *parameters = 1;
+            return 0;
+        }
+    }
+
+    return -1;
 }
